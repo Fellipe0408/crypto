73b27814898f8d9e3ebdd7bf8dea6d2648c32417
Fellipe Gomes <52552307+Fellipe0408@users.noreply.github.com>
2025-05-09 16:38:30 -0300

    Update 09/05

--- crypto/settings.py
+++ crypto/settings.py
@@ -63,8 +63,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             BASE_DIR / 'base',
-            os.path.join(BASE_DIR, 'templates'),
-        ],
+            os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- crypto/urls.py
+++ crypto/urls.py
@@ -13,4 +13,4 @@ urlpatterns = [
     path('formulario/', include('formulario.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+]
